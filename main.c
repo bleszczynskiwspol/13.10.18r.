8ecc6240d5ffa5033a743ba4167de8f3f15e7a39
DanielKubanek <danielkubanek@gmail.com>
2018-10-13 10:04:53 +0200

    układ dwóch równań bez voidow ok

--- main.c
+++ main.c
@@ -13,29 +13,23 @@ int main() {
     double x, y;
 
     printf("program rozwiązuje układ dwóch równań liniowych.\n");
-    printf("Podaj współczynniki.\n");
     printf("a1*x + b1*y = c1\n");
     printf("a2*x + b2*y = c2\n");
+    printf("\n");
     printf("Podaj współczynniki.\n");
 
 
-    printf("podaj a1: ");
-    scanf("%lf", a1);
-    printf("podaj b1: ");
-    scanf("%lf", b1);
-    printf("podaj c1: ");
-    scanf("%lf", c1);
-    printf("podaj a2: ");
-    scanf("%lf", a2);
-    printf("podaj b2: ");
-    scanf("%lf", b2);
-    printf("podaj c2: ");
-    scanf("%lf", c2);
-
+    printf("podaj a1: ");scanf("%lf", &a1);
+    printf("podaj b1: ");scanf("%lf", &b1);
+    printf("podaj c1: ");scanf("%lf", &c1);
+    printf("podaj a2: ");scanf("%lf", &a2);
+    printf("podaj b2: ");scanf("%lf", &b2);
+    printf("podaj c2: ");scanf("%lf", &c2);
+    printf("\n");
 
     printf("twoj układ równań:\n");
-    printf("%5.2lf*x + %5.2lf", a1, b1, c1);
-    printf("%5.2lf*x + %5.2lf", a2, b2, c2);
+    printf("%5.2lf*x + %5.2lf*y = %5.2lf\n", a1, b1, c1);
+    printf("%5.2lf*x + %5.2lf*y = %5.2lf\n", a2, b2, c2);
     printf("\n");
 
     W = wyznacznik(a1, a2, b1, b2);
@@ -43,24 +37,25 @@ int main() {
     Wy = wyznacznik(a1, a2, c1, c2);
 
 
-    printf("wyznacznik W = %lf", W);
-    printf("wyznacznik Wx = %lf", Wx);
-    printf("wyznacznik Wy = %lf", Wy);
+    printf("wyznacznik W = %lf\n", W);
+    printf("wyznacznik Wx = %lf\n", Wx);
+    printf("wyznacznik Wy = %lf\n", Wy);
+    printf("\n");
 
 
-    if(W !=0){
+    if(W !=0.0){
         x = Wx / W;
         y = Wy / W;
         printf("rozwiązanie:\n");
-        printf("x = %5.2lf, y = %5.2lf", x y);
+        printf("x = %5.2lf, y = %5.2lf", x, y);
         printf("\n");
     } else{
-        if(Wx !=0 || Wy != 0){
+        if(Wx !=0 || Wy != 0) {
             printf("układ równań sprzeczny - nie ma rozwiązania.");
-            else{
+        }else{
                 printf("układ równań nieoznaczony - nieskończenie wielerozwiązań.");
             }
-        }
+
     }
 
 
